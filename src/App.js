2722f0836a21ac5f755db5fe04955f148fdf7dca
domsdoms <dominicpunladomingo120@gmail.com>
2023-09-16 17:23:49 +0800

    #changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://simple-chat-apps.onrender.com:3008/");
 
 function App() {
   const [username, setUsername] = useState("");
