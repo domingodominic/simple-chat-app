bf01bc208df2242c42a3693589a43c03af38a9c0
domsdoms <dominicpunladomingo120@gmail.com>
2023-09-16 19:11:07 +0800

    change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import "./App.css";
 import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
-const socket = io.connect("https://chat-app-4o70.onrender.com:3008/");
+
+// Connect to the server using the appropriate URL
+const socket = io.connect("http://localhost:3007");
 
 function App() {
   const [username, setUsername] = useState("");
